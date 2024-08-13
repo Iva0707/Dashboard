2997d229d3c6659cc31e287c561c978722cd2b1d
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-08-13 02:30:13 +0200

    media

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,45 @@
-  
+    //___________header______________
+
+const burger = document.getElementById('burger')
+const menu = document.querySelector('.sidebar')
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    if (burger.classList.contains('active')) {
+        menu.classList.add('show')
+        body.classList.add('overflow_hidden')
+    } else {
+        closeMenu ()
+
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const clickOnMenu = menu.contains(event.target)
+    const clickOnBurger = burger.contains(event.target)
+    if (!clickOnMenu && !clickOnBurger) {
+        closeMenu ()
+    }
+})
+
+menu.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
+        closeMenu ()
+    }
+})
+
+function resize () {
+    const screenWidth = window.innerWidth
+    if (screenWidth > 1199) {
+        closeMenu ()
+    }
+}
+window.addEventListener('resize', resize)
+resize()
+
+function closeMenu () {
+    menu.classList.remove('show')
+    body.classList.remove('overflow_hidden')
+    burger.classList.remove('active')
+}
