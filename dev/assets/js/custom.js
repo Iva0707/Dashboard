bf961afc45bc66c8134e09a6cf0b9942d8105fe4
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-08-13 17:49:22 +0200

    media

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,45 +1,94 @@
-    //___________header______________
+//___________sidebar______________
 
 const burger = document.getElementById('burger')
-const menu = document.querySelector('.sidebar')
+const sidebar = document.querySelector('.sidebar')
 const body = document.querySelector('body')
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     if (burger.classList.contains('active')) {
-        menu.classList.add('show')
-        body.classList.add('overflow_hidden')
+        sidebar.classList.add('show')
+        body.classList.add('body__overflow--hidden')
     } else {
-        closeMenu ()
+        closesidebar ()
 
     }
 })
 
 document.addEventListener('click', (event) => {
-    const clickOnMenu = menu.contains(event.target)
+    const clickOnsidebar = sidebar.contains(event.target)
     const clickOnBurger = burger.contains(event.target)
-    if (!clickOnMenu && !clickOnBurger) {
-        closeMenu ()
+    if (!clickOnsidebar && !clickOnBurger) {
+        closesidebar ()
     }
 })
 
-menu.addEventListener('click', (event) => {
+sidebar.addEventListener('click', (event) => {
     if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
-        closeMenu ()
+        closesidebar ()
     }
 })
 
 function resize () {
     const screenWidth = window.innerWidth
     if (screenWidth > 1199) {
-        closeMenu ()
+        closesidebar ()
     }
 }
 window.addEventListener('resize', resize)
 resize()
 
-function closeMenu () {
-    menu.classList.remove('show')
-    body.classList.remove('overflow_hidden')
+function closesidebar () {
+    sidebar.classList.remove('show')
+    body.classList.remove('body__overflow--hidden')
     burger.classList.remove('active')
 }
+
+//___________nav_list______________
+
+const listItems = document.querySelectorAll('.sidebar__list-item')
+
+listItems[0].addEventListener('click', () => {
+    listItems.forEach(item => item.classList.remove('sidebar__list-item--selected'))
+    // window.location.href = 'index.html';
+    closesidebar()
+})
+
+listItems.forEach((item, index) => {
+    if (index !== 0) {
+        item.addEventListener('click', (event) => {
+            listItems.forEach(i => i.classList.remove('sidebar__list-item--selected'))
+            item.classList.add('sidebar__list-item--selected')
+            event.stopPropagation()
+        })
+    }
+})
+
+document.addEventListener('click', () => {
+    listItems.forEach(item => {
+        item.classList.remove('sidebar__list-item--selected');
+    })
+})
+
+//___________footer_pagination______________
+
+const footerItems = document.querySelectorAll('.content__footer-item')
+const prevButton = document.querySelector('.content__footer-prev')
+const nextButton = document.querySelector('.content__footer-next')
+
+footerItems.forEach(item => {
+    item.addEventListener('click', () => {
+        if (!item.classList.contains('content__footer-item--drops')) {
+            footerItems.forEach(item => item.classList.remove('content__footer-item--active'))
+            item.classList.add('content__footer-item--active')
+        }
+    })
+})
+
+prevButton.addEventListener('click', () => {
+    footerItems.forEach(item => item.classList.remove('content__footer-item--active'))
+})
+
+nextButton.addEventListener('click', () => {
+    footerItems.forEach(item => item.classList.remove('content__footer-item--active'))
+})
